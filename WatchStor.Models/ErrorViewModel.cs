5b35559bf3d84a0ce56d389ae8756f6f6bdd3005
abolfazlshahsavryy <152776561+abolfazlshahsavryy@users.noreply.github.com>
2024-10-18 13:13:01 +0330

    Add project files.

---
+++ WatchStor.Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WatchStor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
